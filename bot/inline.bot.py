43da4a8e4e180b35b266a9232baa82006807a779
Cimera311 <cimerawow@gmail.com>
2024-12-10 18:59:53 +0100

    Update inline.bot.py

--- bot/inline.bot.py
+++ bot/inline.bot.py
@@ -1,1 +1,12 @@
 
+from config import BOT_TOKEN  # Token aus config.py importieren
+
+def main():
+    TOKEN = BOT_TOKEN
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(InlineQueryHandler(inline_query))
+    updater.start_polling()
+    print("Bot läuft... (Drücke STRG+C zum Beenden)")
+    updater.idle()
